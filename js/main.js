85e83e2b517b2d6649007e9ac0816a5a8a080983
yuriygumeniuk <yuriygumeniuk@gmail.com>
2020-01-06 12:19:41 +0200

    class active

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+$(document).ready(function () {
+  $('.nav__link a').click(function(){
+    $(this).parents('.nav-list').find('.nav__link').removeClass('active');
+    $(this).parent().addClass('active');
+    return false
+  });
+
+
 window.scroll({
   top: 0, 
   left: 0, 
   behavior: 'smooth'
+});
+
 });
